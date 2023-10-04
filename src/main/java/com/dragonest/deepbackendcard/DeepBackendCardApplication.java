dc2ab17021e6f98ebff4623b9c5eae823829ce8d
구자현 <110030247+levhyun@users.noreply.github.com>
2023-10-04 14:53:27 +0900

    🎉::Start the DEE:P project of Dragonest.

---
+++ src/main/java/com/dragonest/deepbackendcard/DeepBackendCardApplication.java
@@ -0,0 +1,13 @@
+package com.dragonest.deepbackendcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepBackendCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepBackendCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dragonest/deepbackendcard/DeepBackendCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragonest.deepbackendcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepBackendCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
